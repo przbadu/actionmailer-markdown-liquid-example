97e44fbd409416bfe813433783f5d9095e3c7cad
Pushpa Raj Badu <pushparaj.badu@gmail.com>
2016-06-20 13:44:01 +0545

    add from and reply_to

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "Identity Server <server@freewheeler.com>",
+          reply_to: "Identity Support <support@freewheeler.com>"
   layout 'mailer'
 end
